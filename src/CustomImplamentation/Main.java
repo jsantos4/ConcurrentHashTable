352b523eddf09599fd37fd93941709360cbf48fc
John Santos <jsantos4@oswego.edu>
2018-11-13 14:29:05 -0500

    Minor changes for server testing

--- src/CustomImplamentation/Main.java
+++ src/CustomImplamentation/Main.java
@@ -9,6 +9,9 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    static int threads = (Runtime.getRuntime().availableProcessors() > 32) ? 32 : Runtime.getRuntime().availableProcessors();
+    static int vendors = 4;
+
     public static void main(String[] args) {
 
         Parser p = new Parser();
@@ -30,13 +33,15 @@ public class Main {
         try {
             ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-            for(int i = 0; i < Runtime.getRuntime().availableProcessors() - 1; i++) {
+            for(int i = 0; i < Runtime.getRuntime().availableProcessors() - vendors; i++) {
                 Customer customer = new Customer(i, hashTable);
                 executor.execute(customer);
             }
 
-            Vendor vendor = new Vendor(1, hashTable);
-            executor.execute(vendor);
+            for (int j = 0; j < vendors; j++) {
+                Vendor vendor = new Vendor(j, hashTable);
+                executor.execute(vendor);
+            }
 
             executor.awaitTermination(60, TimeUnit.SECONDS);
             executor.shutdownNow();
