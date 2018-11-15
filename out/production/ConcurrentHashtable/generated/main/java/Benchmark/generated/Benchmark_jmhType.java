58c1d39caa96902595fd05b4f1daca83569b922b
jsantos4 <jsantos4@oswego.edu>
2018-11-14 20:48:07 -0500

    jmh working, need to finish up actual benchmarks.

---
+++ out/production/ConcurrentHashtable/generated/main/java/Benchmark/generated/Benchmark_jmhType.java
@@ -0,0 +1,4 @@
+package main.java.Benchmark.generated;
+public class Benchmark_jmhType extends Benchmark_jmhType_B3 {
+}
+
--- src/main/java/Benchmark/Benchmark.java
+++ src/main/java/Benchmark/Benchmark.java
@@ -3,14 +3,30 @@ package main.java.Benchmark;
 import main.java.CustomImplamentation.*;
 import main.java.Objects.*;
 
-import java.io.IOException;
-import java.util.concurrent.ConcurrentHashMap;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
 public class Benchmark {
     private HashTable hashTable = new HashTable();
     private ConcurrentHashMap<String, Guitar> hashMap = new ConcurrentHashMap<>();
     private Parser parser = new Parser();
 
+    private static int threads = (Runtime.getRuntime().availableProcessors() > 32) ? 32 : Runtime.getRuntime().availableProcessors();
+    private static int vendors = 1;
+    private ArrayList<Customer> customers = new ArrayList<>();
+    private ArrayList<String> catalog = new ArrayList<>();
+    private Random random = new Random();
+
+    @Setup
     public void go() {
         try {
             parser.parse("https://www.normansrareguitars.com/product-category/guitars/", hashTable, hashMap);
@@ -20,8 +36,50 @@ public class Benchmark {
         } catch (IOException e) {
             System.out.println("Could not reach web page.");
         }
+
+        catalog = hashTable.getAll();
+
+        for (int i = 0; i < threads - vendors; i++) {
+            Customer customer = new Customer(i, hashTable);
+            customers.add(customer);
+        }
     }
 
+    @org.openjdk.jmh.annotations.Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void benchCustomGet(Blackhole blackhole) {
+        ExecutorService executor = Executors.newFixedThreadPool(threads);
 
+        for (Customer customer : customers) {
+            executor.execute(customer);
+            executor.execute(customer);
+            executor.execute(customer);
+
+        }
+        executor.shutdown();
+    }
+
+    @org.openjdk.jmh.annotations.Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void benchJDKGet(Blackhole blackhole) {
+        ExecutorService executor = Executors.newFixedThreadPool(threads);
+
+        Runnable c = new Runnable() {
+            @Override
+            public void run() {
+                hashMap.get(catalog.get(random.nextInt(catalog.size())));
+            }
+        };
+
+        for (Customer customer : customers) {
+            executor.execute(c);
+            executor.execute(c);
+            executor.execute(c);
+
+        }
+        executor.shutdown();
+    }
 
 }
